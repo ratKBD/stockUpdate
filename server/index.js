203cbbc810b88fe34c2dc9e9d0c3363665fb6b9f
Vijayan Raghavan <vijayan@Priyeshs-Mac.local>
2024-07-24 14:21:32 +0530

    allow-cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
